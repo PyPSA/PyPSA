eb98ea6468f21861b31558b577c8c0d415e04e5e
Jonas Hoersch <jonas@chaoflow.net>
2016-01-28 14:29:07 +0100

    networkclustering: Add copyright header

--- pypsa/networkclustering.py
+++ pypsa/networkclustering.py
@@ -1,4 +1,25 @@
+## Copyright 2015-2016 Tom Brown (FIAS), Jonas Hoersch (FIAS)
+
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3 of the
+## License, or (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Functions for computing network clusters
+"""
+
 from __future__ import absolute_import
+
+__author__ = "Tom Brown (FIAS), Jonas Hoersch (FIAS)"
+__copyright__ = "Copyright 2015-2016 Tom Brown (FIAS), Jonas Hoersch (FIAS), GNU GPL 3"
 
 import numpy as np
 import pandas as pd
