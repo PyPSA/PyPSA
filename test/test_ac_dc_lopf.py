59d0cc4f6b47f8e930afeb8faa646dd568b20b54
Jonas Hoersch <jonas@chaoflow.net>
2016-10-06 09:20:18 +0200

    test: Adapt test to change of the free_memory semantics

--- test/test_ac_dc_lopf.py
+++ test/test_ac_dc_lopf.py
@@ -37,9 +37,9 @@ def test_lopf():
 
     snapshots = network.snapshots
 
-    for formulation, free_memory_hack in product(["angles", "cycles", "kirchhoff", "ptdf"],
-                                                 [False, True]):
-        network.lopf(snapshots=snapshots,solver_name=solver_name,formulation=formulation, free_memory_hack=free_memory_hack)
+    for formulation, free_memory in product(["angles", "cycles", "kirchhoff", "ptdf"],
+                                            [{}, {"pypsa"}, {"pypsa", "pyomo-hack"}]):
+        network.lopf(snapshots=snapshots,solver_name=solver_name,formulation=formulation, free_memory=free_memory)
 
         np.testing.assert_array_almost_equal(network.generators_t.p,network_r.generators_t.p)
 
