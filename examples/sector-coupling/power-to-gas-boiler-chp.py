3d395300a53be54e50957f56c837e7274fa732b1
Tom Brown <brown@fias.uni-frankfurt.de>
2016-12-12 20:17:12 +0100

    Update and correct CHP example
    Also added graph of allowed p_heat versus p_elec.

--- examples/sector-coupling/power-to-gas-boiler-chp.py
+++ examples/sector-coupling/power-to-gas-boiler-chp.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 from pyomo.environ import Constraint
 
+#%matplotlib inline
+
 heat = True
 chp = True
 
@@ -28,7 +30,7 @@ network.add("Carrier",
 
 network.add("Carrier",
             "gas",
-            co2_emissions=0.9)
+            co2_emissions=0.2)
 
 
 network.add("Generator",
@@ -37,7 +39,7 @@ network.add("Generator",
             carrier="wind",
             p_nom_extendable=True,
             p_max_pu=[0.,0.2,0.7,0.4],
-            capital_cost=500)
+            capital_cost=1000)
 
 network.add("Load",
             "load",
@@ -50,7 +52,7 @@ network.add("Link",
             "P2G",
             bus0="0",
             bus1="0 gas",
-            efficiency=0.3,
+            efficiency=0.6,
             capital_cost=1000,
             p_nom_extendable=True)
 
@@ -58,8 +60,8 @@ network.add("Link",
             "generator",
             bus0="0 gas",
             bus1="0",
-            efficiency=0.4,
-            capital_cost=200,
+            efficiency=0.468,
+            capital_cost=400,
             p_nom_extendable=True)
 
 
@@ -89,7 +91,7 @@ if heat:
             bus0="0 gas",
             bus1="0 heat",
             efficiency=0.9,
-            capital_cost=100,
+            capital_cost=300,
             p_nom_extendable=True)
     
     network.add("Store",
@@ -101,7 +103,15 @@ if heat:
 
 if heat and chp:
     
+    #Define CHP
+    
     #follows http://www.ea-energianalyse.dk/reports/student-reports/integration_of_50_percent_wind%20power.pdf pages 35-6
+    
+    #which follows http://www.sciencedirect.com/science/article/pii/030142159390282K
+    
+    #NB: feasible area of CHP plotted at end of notebook
+    
+    
     #ratio between max heat output and max electric output
     nom_r = 1.
         
@@ -110,17 +120,26 @@ if heat and chp:
         
     #marginal loss for each additional generation of heat
     c_v = 0.15
-            
+    
+    #Guarantees ISO fuel lines, i.e. fuel consumption p_b0 + p_g0 = constant along p_g1 + c_v p_b1 = constant
     network.links.at["boiler","efficiency"] = network.links.at["generator","efficiency"]/c_v
     
     def extra_functionality(network,snapshots):
 
-        network.model.chp_nom = Constraint(rule=lambda model : nom_r*model.link_p_nom["generator"] == model.link_p_nom["boiler"])
+        #Guarantees heat output and electric output nominal powers are proportional
+        network.model.chp_nom = Constraint(rule=lambda model : network.links.at["generator","efficiency"]*nom_r*model.link_p_nom["generator"] == network.links.at["boiler","efficiency"]*model.link_p_nom["boiler"])
 
+        #Guarantees c_m p_b1  \leq p_g1
         def backpressure(model,snapshot):
-            return c_m*model.link_p["boiler",snapshot] <= model.link_p["generator",snapshot] 
+            return c_m*network.links.at["boiler","efficiency"]*model.link_p["boiler",snapshot] <= network.links.at["generator","efficiency"]*model.link_p["generator",snapshot] 
         
         network.model.backpressure = Constraint(list(snapshots),rule=backpressure)
+        
+        #Guarantees p_g1 +c_v p_b1 \leq p_g1_nom
+        def top_iso_fuel_line(model,snapshot):
+            return model.link_p["boiler",snapshot] + model.link_p["generator",snapshot] <= model.link_p_nom["generator"]
+        
+        network.model.top_iso_fuel_line = Constraint(list(snapshots),rule=top_iso_fuel_line)
         
 else:
     extra_functionality = None
@@ -128,6 +147,20 @@ else:
 network.co2_limit=0.
 network.lopf(network.snapshots, extra_functionality=extra_functionality)
 print("Objective:",network.objective)
+
+network.loads_t.p
+
+network.links.p_nom_opt
+
+#CHP is dimensioned by the heat demand met in three hours when no wind
+4*10./3/network.links.at["boiler","efficiency"]
+
+#elec is set by the heat demand
+28.490028*0.15
+
+network.links_t["p0"]
+
+network.links_t["p1"]
 
 print(pd.DataFrame({attr: network.stores_t[attr]["gas depot"] for attr in ["p","e"]}))
 
@@ -143,13 +176,47 @@ print(network.links.p_nom_opt)
 
 #Calculate the overall efficiency of the CHP
 
-eta_elec = 0.4
-c_v = 0.15
-c_m = 0.75
+eta_elec = network.links.at["generator","efficiency"]
 
 r = 1/c_m
 
 #P_h = r*P_e
 
 print((1+r)/((1/eta_elec)*(1+c_v*r)))
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+t = 0.01
+
+ph = np.arange(0,1.0001,t)
+
+#Graph for the case that max heat output equals max electric output
+
+fig,ax = plt.subplots(1,1)
+
+ax.plot(ph,c_m*ph)
+
+ax.set_xlabel("P_heat_out")
+
+ax.set_ylabel("P_elec_out")
+
+ax.grid(True)
+
+ax.set_xlim([0,1.1])
+ax.set_ylim([0,1.1])
+
+ax.text(0.1,0.7,"Allowed output",color="r")
+
+ax.plot(ph,1-c_v*ph)
+
+for i in range(1,10):
+    k = 0.1*i
+    x = np.arange(0,k/(c_m+c_v),t)
+    ax.plot(x,k-c_v*x,color="g",alpha=0.5)
+    
+ax.text(0.05,0.41,"iso-fuel-lines",color="g",rotation=-7)
+
+ax.fill_between(ph,c_m*ph,1-c_v*ph,facecolor="r",alpha=0.5)
 
