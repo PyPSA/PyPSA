67f9f52f77cc18176565f63f694780a580630646
Jonas Hoersch <jonas@chaoflow.net>
2016-12-15 16:05:06 +0100

    opf: Fix co2 price definition as the negative of the dual

--- pypsa/opf.py
+++ pypsa/opf.py
@@ -975,7 +975,7 @@ def extract_optimisation_results(network, snapshots, formulation="angles"):
 
     if network.co2_limit is not None:
         try:
-            network.co2_price = network.model.dual[network.model.co2_constraint]
+            network.co2_price = - network.model.dual[network.model.co2_constraint]
         except AttributeError, KeyError:
             logger.warning("Could not read out co2_price, although a co2_limit was set")
 
