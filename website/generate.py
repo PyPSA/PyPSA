dfa53f289604c3dbe370c98c81e8a1296cd85993
Tom Brown <brown@fias.uni-frankfurt.de>
2017-07-06 12:57:53 +0200

    website: Minor updates

--- website/generate.py
+++ website/generate.py
@@ -76,7 +76,7 @@ PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
 \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
 <html>
 <head>
-<title>{} | nworbmot:tombrown</title>
+<title>pypsa | {}</title>
 <link rel=\"stylesheet\" type=\"text/css\" href=\"{}\" />
 </head>
 <body>
