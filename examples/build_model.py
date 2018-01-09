4d36c1be0820e7b87b4f7dc8d85af08d2deffcf9
Russell Smith <russell.smith@edisonenergy.com>
2018-01-09 14:06:23 -0500

    updating build model example to include links between random linese

--- examples/build_model.py
+++ examples/build_model.py
@@ -12,6 +12,7 @@ from pypsa.opf import network_lopf_build_model as build_model
 
 import argparse
 import logging
+import random
 
 parser = argparse.ArgumentParser()
 
@@ -25,6 +26,7 @@ parser.add_argument(
     help="Print lots of debugging statements",
     action="store_const", dest="loglevel", const=logging.DEBUG,
     default=logging.WARNING,
+
 )
 args = parser.parse_args()
 logging.basicConfig( level=args.loglevel)
@@ -35,31 +37,52 @@ logging.basicConfig( level=args.loglevel)
 
 for test_i in range(1):
 
-    snapshots = range( 1, 1001)
+    snapshots = range( 1, 101)
     p_set = [ p*20 for p in snapshots]
 
     nu = pypsa.Network()
 
     nu.set_snapshots(snapshots)
 
-    nu.add("Bus","bus")
 
-    generator_p_nom = [ i for i in range( 100)]
-    generator_marginal_cost = [ 1000 - i for i in range(100) ]
+    n_gen = 100
+
+    generator_p_nom = [ i for i in range( n_gen)]
+    generator_marginal_cost = [ 1000 - i for i in range( n_gen)]
     p_min_pu = .3
 
     for gen_i, gen in enumerate( generator_p_nom):
+        nu.add("Bus","bus" + str(gen_i))
+        nu.add("Load","load" + str(gen_i),bus= "bus" + str(gen_i),
+               p_set= generator_p_nom[gen_i] / 2. )#[4000,6000,5000,800])
+
         nu.add( "Generator",
                 "gas_" + str(gen_i),
-                bus = "bus",
+                bus = "bus" + str(gen_i),
                 committable = True,
                 p_min_pu = p_min_pu,
                 marginal_cost = generator_marginal_cost[gen_i],
                 p_nom = generator_p_nom[gen_i],)
+        if gen_i > 0:
+            nu.add("Line",
+                   "{} - {} line".format( gen_i - 1, gen_i),
+                   bus0= "bus" + str(gen_i - 1),
+                   bus1= "bus" + str(gen_i),
+                   x=0.1,
+                   r=0.01)
+            random_gen_to_connect = random.randint( 0, n_gen - 1)
+            nu.add("Line",
+                   "{} - {} line".format( gen_i, random_gen_to_connect),
+                   bus0= "bus" + str(random_gen_to_connect),
+                   bus1= "bus" + str(gen_i),
+                   x = .2,
+                   r = .08)
 
-    nu.add("Load","load",bus="bus",p_set= p_set )#[4000,6000,5000,800])
+    print( nu.branches())
 
-    build_model( nu, nu.snapshots, formulation = "kirchoff")
+    build_model( nu, nu.snapshots, formulation = "kirchhoff")
+#    build_model( nu, nu.snapshots, formulation = "angles")
+#    nu.lopf( nu.snapshots, formulation = "kirchhoff")
 
     nu.generators_t.status
 
@@ -90,4 +113,4 @@ for test_i in range(1):
 
     nu.add("Load","load",bus="bus",p_set= p_set )#[4000,800,5000,3000])
 
-    build_model( nu, nu.snapshots, formulation = "kirchoff")
+    build_model( nu, nu.snapshots, formulation = "kirchhoff")
