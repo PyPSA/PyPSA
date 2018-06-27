c954fd6c2e6e3d5fcc5f97928d39a8b28754f67e
WolfBunke <wolf-dieter.bunke@uni-flensburg.de>
2018-06-27 14:05:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst', encoding='utf-8') as f:
 
 setup(
     name='pypsa',
-    version='0.11.0',
+    version='0.11.0fork',
     author='Tom Brown (FIAS), Jonas Hoersch (FIAS), David Schlachtberger (FIAS)',
     author_email='brown@fias.uni-frankfurt.de',
     description='Python for Power Systems Analysis',
