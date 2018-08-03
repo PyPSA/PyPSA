cfa641386e78b6b80f525d636ef4dc2450927568
Jonas Hörsch <hoersch@fias.uni-frankfurt.de>
2018-08-03 12:46:42 +0200

    build_cutout: Fix cutout generation

--- scripts/build_cutout.py
+++ scripts/build_cutout.py
@@ -5,8 +5,13 @@ logger = logging.getLogger(__name__)
 
 logging.basicConfig(level=snakemake.config['logging_level'])
 
+cutout_params = snakemake.config['atlite']['cutouts'][snakemake.wildcards.cutout]
+for p in ('xs', 'ys', 'years', 'months'):
+    if p in cutout_params:
+        cutout_params[p] = slice(*cutout_params[p])
+
 cutout = atlite.Cutout(snakemake.wildcards.cutout,
-                       cutout_dir=os.path.dirname(snakemake.output.cutout),
-                       **snakemake.config['atlite']['cutouts'][snakemake.wildcards.cutout])
+                       cutout_dir=os.path.dirname(snakemake.output[0]),
+                       **cutout_params)
 
 cutout.prepare(nprocesses=snakemake.config['atlite'].get('nprocesses', 4))
