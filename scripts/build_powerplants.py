e7d007bc8fa2355ddd075e5468b76c3b6627d136
Jonas Hörsch <jonas.hoersch@kit.edu>
2018-11-12 21:40:05 +0100

    build_powerplants: Assign power plants only within the right country
    Also update the delivered powerplants snapshots.

--- scripts/build_powerplants.py
+++ scripts/build_powerplants.py
@@ -1,11 +1,20 @@
 # coding: utf-8
 
 import logging
+import numpy as np
 import pandas as pd
 from scipy.spatial import cKDTree as KDTree
+import pycountry as pyc
 
 import pypsa
 import powerplantmatching as ppm
+
+def country_alpha_2(name):
+    try:
+        cntry = pyc.countries.get(name=name)
+    except KeyError:
+        cntry = pyc.countries.get(official_name=name)
+    return cntry.alpha_2
 
 if 'snakemake' not in globals():
     from vresutils.snakemake import MockSnakemake, Dict
@@ -25,16 +34,32 @@ ppl = (ppm.collection.matched_data()
        .assign(Fueltype=lambda df: (
            df.Fueltype.where(df.Fueltype != 'Natural Gas',
                              df.Technology.replace('Steam Turbine', 'OCGT').fillna('OCGT'))))
-       .pipe(ppm.utils.fill_geoposition, parse=True, only_saved_locs=True)
-       .pipe(ppm.heuristics.fill_missing_duration))
+       .pipe(ppm.utils.fill_geoposition))
 
 # ppl.loc[(ppl.Fueltype == 'Other') & ppl.Technology.str.contains('CCGT'), 'Fueltype'] = 'CCGT'
 # ppl.loc[(ppl.Fueltype == 'Other') & ppl.Technology.str.contains('Steam Turbine'), 'Fueltype'] = 'CCGT'
 
 ppl = ppl.loc[ppl.lon.notnull() & ppl.lat.notnull()]
 
-substation_lv_i = n.buses.index[n.buses['substation_lv']]
-kdtree = KDTree(n.buses.loc[substation_lv_i, ['x','y']].values)
-ppl = ppl.assign(bus=substation_lv_i[kdtree.query(ppl[['lon','lat']].values)[1]])
+ppl_country = ppl.Country.map(country_alpha_2)
+countries = n.buses.country.unique()
+cntries_without_ppl = []
+
+for cntry in countries:
+    substation_lv_i = n.buses.index[n.buses['substation_lv'] & (n.buses.country == cntry)]
+    ppl_b = ppl_country == cntry
+    if not ppl_b.any():
+        cntries_without_ppl.append(cntry)
+        continue
+
+    kdtree = KDTree(n.buses.loc[substation_lv_i, ['x','y']].values)
+    ppl.loc[ppl_b, 'bus'] = substation_lv_i[kdtree.query(ppl.loc[ppl_b, ['lon','lat']].values)[1]]
+
+if cntries_without_ppl:
+    logging.warning("No powerplants known in: {}".format(", ".join(cntries_without_ppl)))
+
+bus_null_b = ppl["bus"].isnull()
+if bus_null_b.any():
+    logging.warning("Couldn't find close bus for {} powerplants".format(bus_null_b.sum()))
 
 ppl.to_csv(snakemake.output[0])
