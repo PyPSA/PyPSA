1f84cb0a08c230e05875eb2d6f69f654f750202d
Fabian <fab.hof@gmx.de>
2019-11-12 14:53:28 +0100

    linopf.py handle permission error for windows

--- pypsa/linopf.py
+++ pypsa/linopf.py
@@ -561,8 +561,9 @@ def prepare_lopf(n, snapshots=None, keep_files=False,
         for f in [objective_fn, constraints_fn, bounds_fn]:
             with open(f,'rb') as fd:
                 shutil.copyfileobj(fd, wfd)
-            if not keep_files:
-                os.remove(f)
+    if not keep_files:
+        for f in [objective_fn, constraints_fn, bounds_fn]:
+            os.remove(f)
 
     logger.info(f'Total preparation time: {round(time.time()-start, 2)}s')
 
