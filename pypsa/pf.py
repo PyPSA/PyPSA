7ac0a30489685fdf920e0d3c8a85487cbd0b9cda
Tobias Dess <tobias.dess@elena-international.com>
2020-03-03 20:37:42 +0100

    Transformer Model: Refactored Implementation of off nominal turn ratio and tested it against NEPLAN
    Signed-off-by: Tobias Dess <tobias.dess@elena-international.com>

--- pypsa/pf.py
+++ pypsa/pf.py
@@ -685,12 +685,6 @@ def apply_transformer_types(network):
     t = (network.transformers.loc[trafos_with_types_b, ["bus0", "bus1", "type", "tap_position", "num_parallel"]]
          .join(network.transformer_types, on='type'))
 
-    t = t.merge(network.buses.loc[:, "v_nom"], how='left', left_on="bus0", right_index=True, suffixes=('_bus_0', '_bus_1'))
-    t = t.merge(network.buses.loc[:, "v_nom"], how='left', left_on="bus1", right_index=True, suffixes=('_bus_0', '_bus_1'))
-
-    t["off_nomial_tap_ratio"] = (t["v_nom_0"]/t["v_nom_bus_0"])/(t["v_nom_1"]/t["v_nom_bus_1"])
-    t["off_nomial_voltage_level_winding_1"] = t["v_nom_1"]/t["v_nom_bus_1"]
-
     t["r"] = t["vscr"] /100.
     t["x"] = np.sqrt((t["vsc"]/100.)**2 - t["r"]**2)
 
@@ -709,9 +703,21 @@ def apply_transformer_types(network):
     #deal with tap positions
     t["tap_ratio"] = (1. + (t["tap_position"] - t["tap_neutral"]) * (t["tap_step"]/100.))
 
+    #determine off-nominal tab ratio
+    t = t.merge(network.buses.loc[:, "v_nom"], how='left', left_on="bus0", right_index=True, suffixes=('_bus_0', '_bus_1'))
+    t = t.merge(network.buses.loc[:, "v_nom"], how='left', left_on="bus1", right_index=True, suffixes=('_bus_0', '_bus_1'))
+    t["off_nomial_tap_ratio"] = (t["v_nom_0"] / t["v_nom_bus_0"]) / (t["v_nom_1"] / t["v_nom_bus_1"])
+
+    #correct the admitance with the off nominal voltage level of winding 1 (secondary side)
+    #(the implementation delivers testcase results consistent with NEPLAN 10.8.2.4, but the method is questionable)
+    t["off_nomial_voltage_level_winding_1"] = t["v_nom_1"] / t["v_nom_bus_1"]
+    t["r"] = t["r"]*t["off_nomial_voltage_level_winding_1"]**2
+    t["x"] = t["x"]*t["off_nomial_voltage_level_winding_1"]**2
+    t["g"] = t["g"]/t["off_nomial_voltage_level_winding_1"]**2
+    t["b"] = t["b"]/t["off_nomial_voltage_level_winding_1"]**2
+#
     # now set calculated values on live transformers
-    for attr in ["r", "x", "g", "b", "phase_shift", "s_nom", "tap_side", "tap_ratio", "off_nomial_tap_ratio",
-                 "off_nomial_voltage_level_winding_1"]:
+    for attr in ["r", "x", "g", "b", "phase_shift", "s_nom", "tap_side", "tap_ratio", "off_nomial_tap_ratio"]:
         network.transformers.loc[trafos_with_types_b, attr] = t[attr]
 
     #TODO: status, rate_A
@@ -947,21 +953,16 @@ def calculate_Y(sub_network,skip_pre=False):
     y_sh = branches["g_pu"]+ 1.j*branches["b_pu"]
 
     tau = branches["tap_ratio"].fillna(1.)
-    off_nom_tap_ratio = branches["off_nomial_tap_ratio"].fillna(1.)
-    off_nomial_voltage_level_winding_1 = branches["off_nomial_voltage_level_winding_1"].fillna(1.)
-
-    #correct the admitance with the off nominal voltage level of winding 1 (secondary side)
-    #(the implementation delivers testcase results consistent with NEPLAN 10.8.2.4, but the method is questionable)
-    y_se = y_se/off_nomial_voltage_level_winding_1**2
-    y_sh = y_sh/off_nomial_voltage_level_winding_1**2
 
     #catch some transformers falsely set with tau = 0 by pypower
     tau[tau==0] = 1.
 
     #define the HV tap ratios
     tau_hv = pd.Series(1., branches.index)
-
     tau_hv[branches.tap_side==0] = tau[branches.tap_side==0]
+
+    #take off nominal tap ratio into account
+    off_nom_tap_ratio = branches["off_nomial_tap_ratio"].fillna(1.)
     tau_hv = tau_hv*off_nom_tap_ratio
 
     #define the LV tap ratios
