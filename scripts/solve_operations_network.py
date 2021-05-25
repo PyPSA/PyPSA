e0215bc5a9bfa2341504873d57a2d7019329eadf
Koen van Greevenbroek <74298901+koen-vg@users.noreply.github.com>
2021-05-25 15:55:23 +0200

    Propagate the solver log file name to the solver (#247)
    * Propagate the solver log file name to the solver
    
    Previously, the PyPSA network solving functions were not told about
    the solver logfile specified in the Snakemake file.
    
    * Pass solver_logfile on as kwargs
    
    The `solve_network` function passes any additional arguments on to the
    pypsa `network_lopf` and `ilopf` functions. Now we also pass
    `solver_logfile` on as part of kwargs.

--- scripts/solve_operations_network.py
+++ scripts/solve_operations_network.py
@@ -111,8 +111,9 @@ if __name__ == "__main__":
     fn = getattr(snakemake.log, 'memory', None)
     with memory_logger(filename=fn, interval=30.) as mem:
         n = prepare_network(n, solve_opts=snakemake.config['solving']['options'])
-        n = solve_network(n, config, solver_dir=tmpdir,
-                          solver_log=snakemake.log.solver, opts=opts)
+        n = solve_network(n, config=config, opts=opts,
+                          solver_dir=tmpdir,
+                          solver_logfile=snakemake.log.solver)
         n.export_to_netcdf(snakemake.output[0])
 
     logger.info("Maximum memory usage: {}".format(mem.mem_usage))
