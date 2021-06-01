9825fa32b24a1018472555d66cb8a0615278353c
martacki <martha.frysztacki@kit.edu>
2021-06-01 12:00:55 +0200

    change layout of configuration settings

--- scripts/simplify_network.py
+++ scripts/simplify_network.py
@@ -399,7 +399,7 @@ if __name__ == "__main__":
 
     busmaps = [trafo_map, simplify_links_map, stub_map]
 
-    if snakemake.config['simplify_to_substations']:
+    if snakemake.config['clustering']['simplify']['to_substations']:
         n, substation_map = aggregate_to_substations(n)
         busmaps.append(substation_map)
 
