bf0c22b547748cb9abe20c467d09876addd44b91
Fabian <fab.hof@gmx.de>
2021-06-16 15:43:37 +0200

    add docstrings

--- pypsa/examples.py
+++ pypsa/examples.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 16 11:03:35 2021
-
-@author: fabian
+This module contains functions for retrieving/loading example networks provided
+by the PyPSA project.
 """
 
 import logging
-import urllib
+from urllib.request import urlretrieve as _retrieve
 import os
 
 from pathlib import Path
@@ -28,21 +27,34 @@ _data_dir.mkdir(exist_ok=True)
 
 
 # TODO: replace ending by 'master/' as soon as merging into master
-repo_url = "https://github.com/PyPSA/PyPSA/raw/example-module/"
+_repo_url = "https://github.com/PyPSA/PyPSA/raw/example-module/"
 
 
 def _retrieve_if_not_local(name, repofile, update=False):
     path = (_data_dir / name).with_suffix(".nc")
 
     if not path.exists() or update:
-        url = f"{repo_url}/{repofile}"
+        url = f"{_repo_url}/{repofile}"
         logger.info(f"Retrieving network data from {url}")
-        urllib.request.urlretrieve(url, path)
+        _retrieve(url, path)
 
     return str(path)
 
 
 def ac_dc_meshed(update=False):
+    """
+    Load the meshed AC-DC network example of pypsa stored in the PyPSA repository.
+
+    Parameters
+    ----------
+    update : bool, optional
+        Whether to update the locally stored network data. The default is False.
+
+    Returns
+    -------
+    pyspa.Network
+
+    """
     name = "ac-dc-meshed"
     repofile = "examples/ac-dc-meshed/ac-dc-data.nc"
     path = _retrieve_if_not_local(name, repofile, update=update)
@@ -50,6 +62,19 @@ def ac_dc_meshed(update=False):
 
 
 def storage_hvdc(update=False):
+    """
+    Load the storage network example of pypsa stored in the PyPSA repository.
+
+    Parameters
+    ----------
+    update : bool, optional
+        Whether to update the locally stored network data. The default is False.
+
+    Returns
+    -------
+    pyspa.Network
+
+    """
     name = "storage-hvdc"
     repofile = "examples/opf-storage-hvdc/storage-hvdc.nc"
     path = _retrieve_if_not_local(name, repofile, update=update)
@@ -57,8 +82,20 @@ def storage_hvdc(update=False):
 
 
 def scigrid_de(update=False):
+    """
+    Load the SciGrid network example of pypsa stored in the PyPSA repository.
+
+    Parameters
+    ----------
+    update : bool, optional
+        Whether to update the locally stored network data. The default is False.
+
+    Returns
+    -------
+    pyspa.Network
+
+    """
     name = "scigrid-de"
     repofile = "examples/scigrid-de/scigrid-with-load-gen-trafos.nc"
     path = _retrieve_if_not_local(name, repofile, update=update)
     return Network(path)
-
