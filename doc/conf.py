146609ae958e30b0a7c1118114fd0a054423cfd4
henri_chat_noir <john.richter@adger-analytics>
2021-10-13 12:06:55 -0400

    Add nbsphinx_allow_errors = True statement to conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -330,3 +330,5 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+nbsphinx_allow_errors = True
