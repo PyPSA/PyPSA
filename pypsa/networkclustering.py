d6d6770a453d3d29d7c7372e2b6c859efe395480
Martha Frysztacki <martha.frysztacki@kit.edu>
2022-04-11 16:11:16 +0200

    Update pypsa/networkclustering.py
    Co-authored-by: Fabian Hofmann <hofmann@fias.uni-frankfurt.de>

--- pypsa/networkclustering.py
+++ pypsa/networkclustering.py
@@ -933,7 +933,7 @@ def busmap_by_greedy_modularity(network, n_clusters, buses_i=None):
 
     lines = network.lines.query("bus0 in @buses_i and bus1 in @buses_i")
     lines = (
-        lines.loc[:, ["bus0", "bus1"]]
+        lines[["bus0", "bus1"]]
         .assign(weight=network.lines.s_nom / abs(lines.r + 1j * lines.x))
         .set_index(["bus0", "bus1"])
     )
