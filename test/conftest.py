4d6d0499f231e439dfcb1cb32b11d60799b2219e
Max Parzen <max.parzen@ed.ac.uk>
2022-10-29 19:05:13 +0100

    build custom pandapower network in conftest

--- test/conftest.py
+++ test/conftest.py
@@ -13,6 +13,7 @@ import pandas as pd
 import pytest
 
 import pypsa
+import pandapower as pp
 
 SUPPORTED_APIS = ["pyomo", "linopy", "native"]
 SOLVER_NAME = "glpk"
@@ -59,3 +60,23 @@ def ac_dc_network_multiindexed(ac_dc_network):
     gens_i = n.generators.index
     n.generators_t.p[gens_i] = np.random.rand(len(n.snapshots), len(gens_i))
     return n
+
+
+@pytest.fixture(scope="module")
+def pandapower_network():
+    # more complicated examples like, net = pandapower.networks.example_simple()
+    # can be used once the import of e.g. switches is perfected.
+    # create empty net
+    net = pp.create_empty_network()
+    # create buses
+    bus1 = pp.create_bus(net, vn_kv=20., name="Bus 1")
+    bus2 = pp.create_bus(net, vn_kv=0.4, name="Bus 2")
+    bus3 = pp.create_bus(net, vn_kv=0.4, name="Bus 3")
+    # create bus elements
+    pp.create_ext_grid(net, bus=bus1, vm_pu=1.02, name="Grid Connection")
+    pp.create_load(net, bus=bus3, p_mw=0.100, q_mvar=0.05, name="Load")
+    pp.create_shunt(net, bus=bus3, p_mw=0, q_mvar=0.005, name='Shunt')
+    # create branch elements
+    pp.create_transformer(net, hv_bus=bus1, lv_bus=bus2, std_type="0.4 MVA 20/0.4 kV", name="Trafo")
+    pp.create_line(net, from_bus=bus2, to_bus=bus3, length_km=0.1, std_type="NAYY 4x50 SE", name="Line")
+    return net
