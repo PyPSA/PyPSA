0357ac9b1c43e56ed63ffb2e126f14c26030d650
Max Parzen <max.parzen@ed.ac.uk>
2022-10-29 19:11:16 +0100

    pandapower test case is imported from conftest

--- test/test_pf_against_pandapower.py
+++ test/test_pf_against_pandapower.py
@@ -6,36 +6,13 @@ from numpy.testing import assert_array_almost_equal as equal
 import pypsa
 
 
-def test_pandapower_case():
-    # more complicated examples like
-    # net = pandapower.networks.example_simple()
-    # can be used once the import of e.g. switches is perfected
-
-    # create empty net
-    net = pp.create_empty_network()
-
-    # create buses
-    b1 = pp.create_bus(net, vn_kv=20.0, name="Bus 1")
-    b2 = pp.create_bus(net, vn_kv=0.4, name="Bus 2")
-    b3 = pp.create_bus(net, vn_kv=0.4, name="Bus 3")
-
-    # create bus elements
-    pp.create_ext_grid(net, bus=b1, vm_pu=1.02, name="Grid Connection")
-    pp.create_load(net, bus=b3, p_mw=0.1, q_mvar=0.05, name="Load")
-
-    # create branch elements
-    pp.create_transformer(
-        net, hv_bus=b1, lv_bus=b2, std_type="0.4 MVA 20/0.4 kV", name="Trafo"
-    )
-    pp.create_line(
-        net, from_bus=b2, to_bus=b3, length_km=0.1, name="Line", std_type="NAYY 4x50 SE"
-    )
-
+def test_pandapower_case(pandapower_network):
+    net = pandapower_network
+    # drop shunt component due to bug, powerflow would not match
+    net.shunt = net.shunt.drop(0)
     # because of phase angles, need to init with DC
     pp.runpp(net, calculate_voltage_angles=True, init="dc")
-
     n = pypsa.Network()
-
     n.import_from_pandapower_net(net)
 
     # seed PF with LPF solution because of phase angle jumps
