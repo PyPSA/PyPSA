ef28f86f34f7d135af04389ddd6a9b5646a3ed8b
Fabian Neumann <fabian.neumann@outlook.de>
2023-02-15 10:12:53 +0100

    release v0.22.1

--- pypsa/__init__.py
+++ pypsa/__init__.py
@@ -25,7 +25,7 @@ from pypsa import (
 )
 from pypsa.components import Network, SubNetwork
 
-__version__ = "0.22.0"
+__version__ = "0.22.1"
 
 __author__ = (
     "PyPSA Developers, see https://pypsa.readthedocs.io/en/latest/developers.html"
