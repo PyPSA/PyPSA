b31ec99ee498cd6affb6a4322979adac62823cfd
Fabian <fab.hof@gmx.de>
2024-05-02 11:19:54 +0200

    expressions: fix get_grouper

--- test/test_lopf_expressions.py
+++ test/test_lopf_expressions.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
 import pytest
 
 import pypsa
@@ -17,4 +18,28 @@ def prepared_network(ac_dc_network):
 def test_statistics_capex_by_carrier(ac_dc_network):
     # n = ac_dc_network
     n = pypsa.examples.ac_dc_meshed()
+    n.optimize.create_model()
     n.optimize.statistic_expressions.capex()
+
+
+n = pypsa.examples.ac_dc_meshed()
+n.optimize.create_model()
+s = n.optimize.statistic_expressions
+g = s.groupers
+
+s.capex(groupby=None)
+s.capex(groupby=False)
+s.capex(groupby=False, comps="Link")
+s.capex(groupby=g.get_bus_and_carrier)
+s.capex(bus_carrier="AC")
+# still broken
+# s.capex(groupby=g.get_name_bus_and_carrier)
+
+
+from linopy import merge
+
+data = {k: d[k].indexes[d[k].dims[0]] for k in d.keys()}
+tuples = [(key, value) for key, idx in data.items() for value in idx]
+multi_index = pd.MultiIndex.from_tuples(tuples, names=["Type", "Carrier"])
+
+merge(list(d.values()), dim="carrier")
