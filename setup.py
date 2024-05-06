d73da0a0ba2d88f011816399cd6a97da7a2aa543
Fabian <fab.hof@gmx.de>
2024-05-06 13:04:05 +0200

    setup.py: update linopy version dep

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "netcdf4",
         "tables",
         "pyomo>=5.7,<6.6.2; python_version < '3.12'",
-        "linopy>=0.3.7",
+        "linopy>=0.3.9",
         "matplotlib",
         "geopandas>=0.9",
         "networkx>=2",
