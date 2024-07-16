5043fc291c7958583222959901c483f011bf97d4
Philipp Glaum <p.glaum@tu-berlin.de>
2024-07-16 12:01:34 +0200

    spatial.py: add custom groupby argument to clustering which allows you to add columns considered in the gouping of branches besides bus0 and bus1

--- pypsa/clustering/spatial.py
+++ pypsa/clustering/spatial.py
@@ -322,6 +322,7 @@ def aggregatelines(
     with_time=True,
     custom_strategies=None,
     bus_strategies=None,
+    custom_line_groupers=[],
 ):
     """
     Aggregate lines in the network based on the given busmap.
@@ -375,7 +376,7 @@ def aggregatelines(
     strategies = {**DEFAULT_LINE_STRATEGIES, **custom_strategies}
     static_strategies = align_strategies(strategies, columns, "Line")
 
-    grouper = df.groupby(["bus0", "bus1"]).ngroup().astype(str)
+    grouper = df.groupby(["bus0", "bus1", *custom_line_groupers]).ngroup().astype(str)
 
     coords = buses[["x", "y"]]
     length = (
@@ -455,6 +456,7 @@ def get_clustering_from_busmap(
     generator_strategies=dict(),
     line_strategies=dict(),
     aggregate_generators_buses=None,
+    custom_line_groupers=[],
 ):
     if aggregate_one_ports is None:
         aggregate_one_ports = {}
@@ -468,6 +470,7 @@ def get_clustering_from_busmap(
         with_time=with_time,
         custom_strategies=line_strategies,
         bus_strategies=bus_strategies,
+        custom_line_groupers=custom_line_groupers,
     )
 
     clustered = Network()
