84e46810e3d9aa14f50c5ac2c0f18ab790ce7118
Fabian Hofmann <fab.hof@gmx.de>
2024-08-02 11:22:45 +0200

    __init__.py: fix typo (#997)
    * __init__.py: fix typo
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pypsa/__init__.py
+++ pypsa/__init__.py
@@ -35,9 +35,7 @@ __version__ = version("pypsa")
 # e.g. "0.17.0" # TODO, in the network structure it should use the dev version
 release_version = re.match(r"(\d+\.\d+(\.\d+)?)", __version__).group(0)
 
-assert not __version__.startswith(
-    "0.0"
-), "Could not determine version of powerplantmatching."
+assert not __version__.startswith("0.0"), "Could not determine version of pypsa."
 
 __all__ = [
     "clustering",
