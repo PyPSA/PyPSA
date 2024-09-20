1bcdda73ead59c86d95789e57168e17055575ff3
toniseibold <tseibold.tub@gmail.com>
2024-09-20 09:33:19 +0200

    refining of test

--- test/test_link_post_discretization.py
+++ test/test_link_post_discretization.py
@@ -30,45 +30,50 @@ def build_network(unit_size=10):
         p_nom_extendable=True,
     )
 
-    n.add(
-        "Link",
-        "Link1",
-        bus0="Bus0",
-        bus1="Bus1",
-        p_nom=0,
-        p_nom_opt=1.5 * unit_size,
-        p_nom_max=2 * unit_size,
-        p_nom_extendable=True,
-    )
-    n.add(
-        "Link",
-        "Link2",
-        bus0="Bus0",
-        bus1="Bus1",
-        p_nom=0,
-        p_nom_opt=0.5 * unit_size,
-        p_nom_max=0.8 * unit_size,
-        p_nom_extendable=True,
-    )
-    n.add(
-        "Link",
-        "Link3",
-        bus0="Bus0",
-        bus1="Bus1",
-        p_nom=0,
-        p_nom_opt=1.3 * unit_size,
-        p_nom_max=1.5 * unit_size,
-        p_nom_extendable=True,
-    )
-    n.add(
-        "Link",
-        "Link4",
-        bus0="Bus0",
-        bus1="Bus1",
-        p_nom=0,
-        p_nom_opt=1.5 * unit_size,
-        p_nom_extendable=True,
-    )
+    n.add("Link",
+          "Link1",
+          bus0="Bus0",
+          bus1="Bus1",
+          p_nom=0,
+          p_nom_opt=1.5*unit_size,
+          p_nom_max=2*unit_size,
+          p_nom_extendable=True,
+          )
+    n.add("Link",
+          "Link2",
+          bus0="Bus0",
+          bus1="Bus1",
+          p_nom=0,
+          p_nom_opt=0.5*unit_size,
+          p_nom_max=0.8*unit_size,
+          p_nom_extendable=True,
+          )
+    n.add("Link",
+          "Link3",
+          bus0="Bus0",
+          bus1="Bus1",
+          p_nom=0,
+          p_nom_opt=1.3*unit_size,
+          p_nom_max=1.5*unit_size,
+          p_nom_extendable=True,
+          )
+    n.add("Link",
+          "Link4",
+          bus0="Bus0",
+          bus1="Bus1",
+          p_nom=5,
+          p_nom_opt=1.1*unit_size,
+          p_nom_max=1.5*unit_size,
+          p_nom_extendable=True,
+          )
+    n.add("Link",
+          "Link5",
+          bus0="Bus0",
+          bus1="Bus1",
+          p_nom=0,
+          p_nom_opt=1.5*unit_size,
+          p_nom_extendable=True,
+          )
     return n
 
 
@@ -81,7 +86,6 @@ def test_post_discretization():
     """
 
     unit_size = 10
-    threshold = 0.1
 
     n = build_network(unit_size=unit_size)
 
@@ -90,17 +94,25 @@ def test_post_discretization():
             nom_opt=row["p_nom_opt"],
             nom_max=row["p_nom_max"],
             unit_size=unit_size,
-            threshold=threshold,
+            threshold=0.3,
             fractional_last_unit_size=True,
         ),
         axis=1,
     )
 
-    assert n.links.loc["Link0"].p_nom == unit_size
-    assert n.links.loc["Link1"].p_nom == 2 * unit_size
-    assert n.links.loc["Link2"].p_nom == 0.8 * unit_size
-    assert n.links.loc["Link3"].p_nom == 1.5 * unit_size
-    assert n.links.loc["Link4"].p_nom == 2 * unit_size
+    # p_nom_opt   | p_nom_max | p_nom     | unit_size
+    # 5           | 10        | 10        | 10
+    assert (n.links.loc["Link0"].p_nom == unit_size)
+    # 15          | 20        | 20        | 10
+    assert (n.links.loc["Link1"].p_nom == 2*unit_size)
+    # 5           | 8         | 8         | 10
+    assert (n.links.loc["Link2"].p_nom == 0.8*unit_size)
+    # 13          | 15        | 15        | 10
+    assert (n.links.loc["Link3"].p_nom == 1.5*unit_size)
+    # 11          | 15        | 5         | 10
+    assert (n.links.loc["Link4"].p_nom == unit_size)
+    # 15          | inf       | 20        | 10
+    assert (n.links.loc["Link5"].p_nom == 2*unit_size)
 
     n = build_network(unit_size=unit_size)
 
@@ -109,14 +121,22 @@ def test_post_discretization():
             nom_opt=row["p_nom_opt"],
             nom_max=row["p_nom_max"],
             unit_size=unit_size,
-            threshold=threshold,
+            threshold=0.3,
             fractional_last_unit_size=False,
         ),
         axis=1,
     )
-
-    assert n.links.loc["Link0"].p_nom == unit_size
-    assert n.links.loc["Link1"].p_nom == 2 * unit_size
-    assert n.links.loc["Link2"].p_nom == 0
-    assert n.links.loc["Link3"].p_nom == unit_size
-    assert n.links.loc["Link4"].p_nom == 2 * unit_size
+    
+    # p_nom_opt   | p_nom_max | p_nom     | unit_size
+    # 5           | 10        | 10        | 10
+    assert (n.links.loc["Link0"].p_nom == unit_size)
+    # 15          | 20        | 20        | 10
+    assert (n.links.loc["Link1"].p_nom == 2*unit_size)
+    # 5           | 8         | 0         | 10
+    assert (n.links.loc["Link2"].p_nom == 0)
+    # 13          | 15        | 10        | 10
+    assert (n.links.loc["Link3"].p_nom == unit_size)
+    # 11          | 15        | 10        | 10
+    assert (n.links.loc["Link4"].p_nom == unit_size)
+    # 15          | inf       | 20        | 10
+    assert (n.links.loc["Link5"].p_nom == 2*unit_size)
