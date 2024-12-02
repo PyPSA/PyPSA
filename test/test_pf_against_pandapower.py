432a664a600405a31d1e19cfc8d90ec7e4fa8f46
lkstrp <lkstrp@pm.me>
2024-12-02 15:54:02 +0100

    fix: adjust tests

--- test/test_pf_against_pandapower.py
+++ test/test_pf_against_pandapower.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandapower as pp
 import pytest
 from numpy.testing import assert_array_almost_equal as equal
 
@@ -12,6 +11,8 @@ from pypsa.constants import DEFAULT_TIMESTAMP
 def test_pandapower_custom_case(
     pandapower_custom_network, use_pandapower_index, extra_line_data
 ):
+    import pandapower as pp
+
     net = pandapower_custom_network
     # because of phase angles, need to init with DC
     pp.runpp(net, calculate_voltage_angles=True, init="dc")
