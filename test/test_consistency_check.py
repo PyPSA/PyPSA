c3a3f75cd36e9ba6bc5f427284eddbc1100fd462
Fabian Hofmann <fab.hof@gmx.de>
2024-12-16 13:54:56 +0100

    add strict parameter to consistency check (#1112)
    * add stric parameter to consistency check
    
    * add release notes
    
    * Update pypsa/consistency.py
    
    Co-authored-by: Lukas Trippe <lkstrp@pm.me>
    
    * consistency: reinsert lazy logging attributes
    
    * consistency: add custom error class
    
    ---------
    
    Co-authored-by: Lukas Trippe <lkstrp@pm.me>

--- test/test_consistency_check.py
+++ test/test_consistency_check.py
@@ -11,6 +11,16 @@ import numpy as np
 import pytest
 
 import pypsa
+import pypsa.consistency
+
+
+def assert_log_or_error_in_consistency(n, caplog, strict=False):
+    if strict:
+        with pytest.raises(pypsa.consistency.ConsistencyError):
+            n.consistency_check(strict=strict)
+    else:
+        n.consistency_check(strict=strict)
+        assert caplog.records[-1].levelname == "WARNING"
 
 
 @pytest.fixture
@@ -24,35 +34,40 @@ def consistent_n():
     return n
 
 
+@pytest.mark.parametrize("strict", [True, False])
 @pytest.mark.skipif(os.name == "nt", reason="dtype confusing on Windows")
-def test_consistency(consistent_n, caplog):
-    consistent_n.consistency_check()
-    assert not caplog.records
+def test_consistency(consistent_n, caplog, strict):
+    if strict:
+        consistent_n.consistency_check(strict=strict)
+    else:
+        consistent_n.consistency_check()
+        assert not caplog.records
 
 
-def test_missing_bus(consistent_n, caplog):
+@pytest.mark.parametrize("strict", [True, False])
+def test_missing_bus(consistent_n, caplog, strict):
     consistent_n.add("Bus", "three")
-    consistent_n.consistency_check()
-    assert caplog.records[-1].levelname == "WARNING"
+    assert_log_or_error_in_consistency(consistent_n, caplog, strict=strict)
 
 
-def test_infeasible_capacity_limits(consistent_n, caplog):
+@pytest.mark.parametrize("strict", [True, False])
+def test_infeasible_capacity_limits(consistent_n, caplog, strict):
     consistent_n.generators.loc["gen_one", ["p_nom_extendable", "committable"]] = (
         True,
         True,
     )
-    consistent_n.consistency_check()
-    assert caplog.records[-1].levelname == "WARNING"
+    assert_log_or_error_in_consistency(consistent_n, caplog, strict=strict)
 
 
-def test_nans_in_capacity_limits(consistent_n, caplog):
+@pytest.mark.parametrize("strict", [True, False])
+def test_nans_in_capacity_limits(consistent_n, caplog, strict):
     consistent_n.generators.loc["gen_one", "p_nom_extendable"] = True
     consistent_n.generators.loc["gen_one", "p_nom_max"] = np.nan
-    consistent_n.consistency_check()
-    assert caplog.records[-1].levelname == "WARNING"
+    assert_log_or_error_in_consistency(consistent_n, caplog, strict=strict)
 
 
-def test_shapes_with_missing_idx(ac_dc_network_shapes, caplog):
+@pytest.mark.parametrize("strict", [True, False])
+def test_shapes_with_missing_idx(ac_dc_network_shapes, caplog, strict):
     n = ac_dc_network_shapes
     n.add(
         "Shape",
@@ -61,23 +76,21 @@ def test_shapes_with_missing_idx(ac_dc_network_shapes, caplog):
         component="Bus",
         idx="missing_idx",
     )
-    n.consistency_check()
-    assert caplog.records[-1].levelname == "WARNING"
-    assert caplog.records[-1].message.startswith("The following shapes")
+    assert_log_or_error_in_consistency(ac_dc_network_shapes, caplog, strict=strict)
 
 
-def test_unknown_carriers(consistent_n, caplog):
+@pytest.mark.parametrize("strict", [True, False])
+def test_unknown_carriers(consistent_n, caplog, strict):
     consistent_n.add("Generator", "wind", bus="hub", carrier="wind")
-    consistent_n.consistency_check()
-    assert caplog.records[-1].levelname == "WARNING"
+    assert_log_or_error_in_consistency(consistent_n, caplog, strict=strict)
 
 
-def test_inconsistent_e_sum_values(consistent_n, caplog):
+@pytest.mark.parametrize("strict", [True, False])
+def test_inconsistent_e_sum_values(consistent_n, caplog, strict):
     """
     Test that the consistency check raises a warning if the e_sum_min is greater than e_sum_max.
     """
     consistent_n.add(
         "Generator", "gen_two", bus="one", p_nom_max=10, e_sum_min=10, e_sum_max=5
     )
-    consistent_n.consistency_check()
-    assert caplog.records[-1].levelname == "WARNING"
+    assert_log_or_error_in_consistency(consistent_n, caplog, strict=strict)
