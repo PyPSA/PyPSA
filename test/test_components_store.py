e6d56728f2ae97b9a7628ca51361c91d985be8c2
lkstrp <lkstrp@pm.me>
2025-02-06 11:40:47 +0100

    tests

--- test/test_components_store.py
+++ test/test_components_store.py
@@ -1,3 +1,8 @@
+import warnings
+
+import pytest
+
+import pypsa
 from pypsa.definitions.components import ComponentsStore
 
 
@@ -9,3 +14,21 @@ def test_init():
     components = ComponentsStore({"a": "b", "c": "d"})
     assert components["a"] == "b"
     assert components["c"] == "d"
+
+
+def test_deprecations():
+    components = ComponentsStore()
+
+    with pytest.raises(DeprecationWarning):
+        "c" in components
+
+    with pytest.warns(DeprecationWarning):
+        for c in components:
+            pass
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")  # Convert warnings to errors
+
+        with pypsa.option_context("warnings.components_store_iter", False):
+            for c in components:
+                pass
