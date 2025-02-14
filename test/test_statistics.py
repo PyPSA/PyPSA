751607664cfb78c2dff2ded09a0348bc38a1aafa
Markus Groissboeck <markus.groissboeck@gmx.at>
2025-02-14 12:23:11 +0100

    add carbon emissions to statistics module (issue #520)

--- test/test_statistics.py
+++ test/test_statistics.py
@@ -261,3 +261,16 @@ def test_parameters(ac_dc_network_r):
         n.statistics.set_parameters(round="one")
     # Test parameter representation
     isinstance(n.statistics.parameters, str)
+
+import pypsa
+
+
+def test_emissions_calculation():
+    n = pypsa.examples.ac_dc_meshed()
+    n.optimize(n.snapshots[:14])
+    n.statistics().round(1).T
+    # assert reloaded.meta == scipy_network.meta
+    assert (
+        n.statistics()["Carbon Emission"].sum()
+        == n.global_constraints.constant["co2_limit"]
+    )
