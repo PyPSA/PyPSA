bb14685548a2feb58099ac7c29befd69d5e3628c
Fabian <fab.hof@gmx.de>
2025-03-18 09:39:24 +0100

    rename test_plot_charts

--- test/test_plot_statistics.py
+++ test/test_plot_statistics.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import pytest
-import seaborn.objects as so
+import seaborn as sns
 
 from pypsa.consistency import ConsistencyError
 from pypsa.plot.statistics.charts import (
@@ -9,35 +9,6 @@ from pypsa.plot.statistics.charts import (
     BarPlotGenerator,
     LinePlotGenerator,
 )
-from pypsa.statistics.expressions import StatisticsAccessor
-
-
-@pytest.mark.parametrize("stat_func", StatisticsAccessor._methods)
-def test_bar_plot(ac_dc_network_r, stat_func):
-    plotter = getattr(ac_dc_network_r.statistics, stat_func)
-    plotter.bar()
-    plt.close()
-
-
-@pytest.mark.parametrize("stat_func", StatisticsAccessor._methods)
-def test_line_plot(ac_dc_network_r, stat_func):
-    plotter = getattr(ac_dc_network_r.statistics, stat_func)
-    plotter.line()
-    plt.close()
-
-
-@pytest.mark.parametrize("stat_func", StatisticsAccessor._methods)
-def test_area_plot(ac_dc_network_r, stat_func):
-    plotter = getattr(ac_dc_network_r.statistics, stat_func)
-    plotter.area()
-    plt.close()
-
-
-@pytest.mark.parametrize("stat_func", StatisticsAccessor._methods)
-def test_map_plot(ac_dc_network_r, stat_func):
-    plotter = getattr(ac_dc_network_r.statistics, stat_func)
-    plotter.area()
-    plt.close()
 
 
 def test_to_long_format_static(ac_dc_network_r):
@@ -109,44 +80,6 @@ def test_area_plotter_validation(ac_dc_network_r):
     assert plotter._validate(data).equals(data)
 
 
-def test_process_data_for_stacking(ac_dc_network_r):
-    """Test the _process_data_for_stacking method"""
-    plotter = BarPlotGenerator(ac_dc_network_r)
-
-    # Test with positive and negative values
-    data = pd.DataFrame(
-        {
-            "carrier": ["a", "a", "b", "b"],
-            "value": [1, -1, 2, -2],
-        }
-    )
-    stacked_data = plotter._process_data_for_stacking(data, "carrier")
-    assert len(stacked_data) == 4
-    assert set(stacked_data["value"]) == {1, -1, 2, -2, -1, -2}
-
-
-def test_base_plot_color_scale(ac_dc_network_r):
-    """Test color scale application in base plot"""
-    plotter = BarPlotGenerator(ac_dc_network_r)
-    data = pd.Series([1, 2, 3], index=pd.Index(["a", "b", "c"], name="carrier"))
-    plot = plotter._base_plot(data, x="carrier", y="value", color="carrier")
-    assert isinstance(plot, so.Plot)
-    assert plot._scales["color"] is not None
-
-
-def test_base_plot_faceting(ac_dc_network_r):
-    """Test faceting functionality in base plot"""
-    plotter = BarPlotGenerator(ac_dc_network_r)
-    data = (
-        pd.DataFrame({"carrier": ["a", "b"], "value": [1, 2], "region": ["r1", "r2"]})
-        .set_index(["carrier", "region"])
-        .value
-    )
-    plot = plotter._base_plot(data, x="carrier", y="value", facet_col="region")
-    assert isinstance(plot, so.Plot)
-    assert plot._facet_spec is not None
-
-
 # def test_derive_statistic_parameters(ac_dc_network_r):
 #     """Test derivation of statistic parameters"""
 #     # TODO rewrite once function is updated
@@ -197,8 +130,12 @@ def test_query_filtering(ac_dc_network_r):
     plotter = BarPlotGenerator(ac_dc_network_r)
     data = pd.Series([1, 2, 3], index=pd.Index(["a", "b", "c"], name="carrier"))
 
-    filtered_plot = plotter._base_plot(data, x="carrier", y="value", query="value > 1")
-    assert isinstance(filtered_plot, so.Plot)
+    fig, ax, g = plotter._base_plot(
+        data, "bar", x="carrier", y="value", query="value > 1"
+    )
+    assert isinstance(fig, plt.Figure)
+    assert isinstance(ax, plt.Axes)
+    assert isinstance(g, sns.FacetGrid)
 
 
 def test_consistency_checks(ac_dc_network_r):
@@ -210,21 +147,19 @@ def test_consistency_checks(ac_dc_network_r):
         n = ac_dc_network_r.copy()
         plotter = BarPlotGenerator(n)
         n.carriers.color = pd.Series()
-        plotter._base_plot(data=pd.DataFrame(), x="carrier", y="value")
+        plotter._base_plot(data=pd.DataFrame(), kind="area", x="carrier", y="value")
 
 
 def test_stacking_and_dodging(ac_dc_network_r):
     """Test stacking and dodging options in bar plots"""
     n = ac_dc_network_r
-    stacked_plot = n.statistics.supply.bar(x="carrier", y="value", stacked=True)
-    assert isinstance(stacked_plot, so.Plot)
-
-    # Test dodged plot
-    dodged_plot = n.statistics.supply.bar(x="carrier", y="value", dodged=True)
-    assert isinstance(dodged_plot, so.Plot)
+    fig, ax, g = n.plot.supply.bar(x="carrier", y="value", stacked=True)
+    assert isinstance(fig, plt.Figure)
+    assert isinstance(ax, plt.Axes)
+    assert isinstance(g, sns.FacetGrid)
 
 
 def test_line_plot_resampling(ac_dc_network_r):
     """Test resampling functionality in line plots"""
     n = ac_dc_network_r
-    n.statistics.supply.line(resample="1D", x="snapshot")
+    n.plot.supply.line(resample="1D", x="snapshot")
