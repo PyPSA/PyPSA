f6618a7ef4716bac8cddb38670182f2e5c09d332
lkstrp <lkstrp@pm.me>
2025-04-01 15:39:51 +0200

    add version tests

---
+++ test/test_version.py
@@ -0,0 +1,9 @@
+from pypsa.version import check_pypsa_version
+
+
+def test_version_check(caplog):
+    check_pypsa_version("0.20.0")
+    assert caplog.text == ""
+
+    check_pypsa_version("0.0")
+    assert "The correct version of PyPSA could not be resolved" in caplog.text
