7a660b91f82b8735be1d0a2da6504a3b559c5d0d
lkstrp <lkstrp@pm.me>
2025-04-14 16:59:05 +0200

    fix imported method docstrings for components

--- pypsa/components/descriptors.py
+++ pypsa/components/descriptors.py
@@ -1,61 +1,76 @@
 """
-Abstract components module.
+Components descriptor module.
 
-Contains classes and logic relevant to all component types in PyPSA.
+Contains single helper class (ComponentsDescriptors) which is used to inherit
+to Components class. Should not be used directly.
 """
 
 from __future__ import annotations
 
 import logging
 from collections.abc import Sequence
-from typing import TYPE_CHECKING
+from typing import Any
 
 import numpy as np
 import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-if TYPE_CHECKING:
-    from pypsa import Components
+
+def get_component_type(*args: Any, **kwargs: Any) -> Any:  # noqa: D103
+    msg = (
+        "pypsa.components.descriptors.get_component_type is deprecated. "
+        "Use c.get_component_type instead."
+        "Deprecated in version 0.35 and will be removed in version 1.0."
+    )
+    raise DeprecationWarning(msg)
 
 
-def get_active_assets(
-    c: Components,
-    investment_period: int | str | Sequence | None = None,
-) -> pd.Series:
+class ComponentsDescriptors:
     """
-    Get active components mask of componen type in investment period(s).
+    Helper class for components descriptors methods.
 
-    A component is considered active when:
-    - it's active attribute is True
-    - it's build year + lifetime is smaller than the investment period (if given)
-
-    Parameters
-    ----------
-    c : pypsa.Components
-        Components instance.
-    investment_period : int, str, Sequence
-        Investment period(s) to check for active within build year and lifetime. If
-        none only the active attribute is considered and build year and lifetime are
-        ignored. If multiple periods are given the mask is True if component is
-        active in any of the given periods.
-
-    Returns
-    -------
-    pd.Series
-        Boolean mask for active components
-
+    Class only inherits to Components and should not be used directly.
     """
-    if investment_period is None:
-        return c.static.active
-    if not {"build_year", "lifetime"}.issubset(c.static):
-        return c.static.active
 
-    # Logical OR of active assets in all investment periods and
-    # logical AND with active attribute
-    active = {}
-    for period in np.atleast_1d(investment_period):
-        if period not in c.n_save.investment_periods:
-            raise ValueError("Investment period not in `n.investment_periods`")
-        active[period] = c.static.eval("build_year <= @period < build_year + lifetime")
-    return pd.DataFrame(active).any(axis=1) & c.static.active
+    def get_active_assets(
+        self,
+        investment_period: int | str | Sequence | None = None,
+    ) -> pd.Series:
+        """
+        Get active components mask of componen type in investment period(s).
+
+        A component is considered active when:
+
+        - it's active attribute is True
+        - it's build year + lifetime is smaller than the investment period (if given)
+
+        Parameters
+        ----------
+        investment_period : int, str, Sequence
+            Investment period(s) to check for active within build year and lifetime. If
+            none only the active attribute is considered and build year and lifetime are
+            ignored. If multiple periods are given the mask is True if component is
+            active in any of the given periods.
+
+        Returns
+        -------
+        pd.Series
+            Boolean mask for active components
+
+        """
+        if investment_period is None:
+            return self.static.active
+        if not {"build_year", "lifetime"}.issubset(self.static):
+            return self.static.active
+
+        # Logical OR of active assets in all investment periods and
+        # logical AND with active attribute
+        active = {}
+        for period in np.atleast_1d(investment_period):
+            if period not in self.n_save.investment_periods:
+                raise ValueError("Investment period not in `n.investment_periods`")
+            active[period] = self.static.eval(
+                "build_year <= @period < build_year + lifetime"
+            )
+        return pd.DataFrame(active).any(axis=1) & self.static.active
