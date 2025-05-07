a564919550b59e72400c3c6c859604ef71313bef
Irieo <iegor.riepin@gmail.com>
2025-05-07 16:27:33 +0200

    add full test for stoch problem obj, dispatch, status

--- test/test_stochastic.py
+++ test/test_stochastic.py
@@ -6,6 +6,7 @@ import os
 
 import pandas as pd
 import pytest
+from numpy.testing import assert_array_almost_equal as equal
 
 import pypsa
 
@@ -122,7 +123,7 @@ def test_solved_network_simple(stochastic_benchmark_network):
     if not os.path.exists(benchmark_path):
         pytest.skip("Benchmark data not available")
 
-    # n_r = pypsa.Network(benchmark_path)
+    n_r = pypsa.Network(benchmark_path)
 
     # Create a new network for the stochastic model
     n = stochastic_benchmark_network
@@ -135,21 +136,18 @@ def test_solved_network_simple(stochastic_benchmark_network):
         100 / n.generators.loc[("high", "gas"), "efficiency"]
     )
 
-    n.optimize.create_model()
+    status, _ = n.optimize(solver_name="highs")
+    assert status == "ok"
 
-    # # Check that it solves to optimality
-    # status, _ = n.optimize(solver_name="highs")
-    # assert status == "ok"
+    # Compare generator capacities (these are the main result of stochastic planning)
+    equal(
+        n.generators.p_nom_opt.loc["low", :],
+        n_r.generators.p_nom_opt,
+        decimal=2,
+    )
 
-    # # Compare generator capacities (these are the main result of stochastic planning)
-    # equal(
-    #     n.generators.p_nom_opt,
-    #     n_r.generators.p_nom_opt,
-    #     decimal=2,
-    # )
-
-    # # Compare objective value
-    # equal(n.objective, n_r.objective, decimal=2)
+    # Compare objective value
+    equal(n.objective, n_r.objective, decimal=2)
 
 
 def test_solved_network_advanced():
