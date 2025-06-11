685460d4d9129c0e6e9486c3de7bd9aa7be70969
lkstrp <lkstrp@pm.me>
2025-06-11 15:07:23 +0200

    test: add tests for regex patterns

--- pypsa/constants.py
+++ pypsa/constants.py
@@ -9,4 +9,4 @@ RE_PORTS = re.compile(r"^bus(\d*)$")
 # Pattern for filtering bus columns without capture groups
 RE_PORTS_FILTER = re.compile(r"^bus\d*$")
 # Pattern to get port numbers greater or equal to 2
-RE_PORTS_GE_2 = re.compile(r"^bus([2-9]\d*)$")
+RE_PORTS_GE_2 = re.compile(r"^bus((?:[2-9]|[1-9]\d+))$")
