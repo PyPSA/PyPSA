3bcfeb59579e0f3fd3df48702eb4ee46583cad2a
lkstrp <lkstrp@pm.me>
2025-06-23 13:27:44 +0200

    test: fix logging checks in tests

--- test/test_network_transform.py
+++ test/test_network_transform.py
@@ -102,14 +102,14 @@ class TestGetPotentialTypos:
         # Add a generator attribute 'p_nom' to a bus - this should trigger the warning
         # p_nom is a standard attribute for generators but not for buses
         n.add("Bus", "bus_with_generator_attr", p_nom=100.0)
-        assert "are default attributes for other components" in caplog.text
+        assert "is a standard attribute for other components" in caplog.text
         assert "p_nom" in caplog.text
         caplog.clear()
 
         # Add a bus attribute 'v_nom' to a generator - this should trigger the warning
         # v_nom is a standard attribute for buses but not for generators
         n.add("Generator", "gen_with_bus_attr", bus="test_bus", v_nom=380.0)
-        assert "are default attributes for other components" in caplog.text
+        assert "is a standard attribute for other components" in caplog.text
         assert "v_nom" in caplog.text
 
     def test_potential_typo_warning(self, caplog):
@@ -119,21 +119,21 @@ class TestGetPotentialTypos:
 
         # Add a generator with a typo in p_nom (p_nok)
         n.add("Generator", "gen_with_typo", bus="test_bus", p_nok=100.0)
-        assert "is likely a typo of standard attribute" in caplog.text
+        assert "is not a standard attribute for" in caplog.text
         assert "p_nok" in caplog.text
         assert "p_nom" in caplog.text
         caplog.clear()
 
         # Add a bus with a typo in v_nom (v_nok)
         n.add("Bus", "bus_with_typo", v_nok=380.0)
-        assert "is likely a typo of standard attribute" in caplog.text
+        assert "is not a standard attribute for" in caplog.text
         assert "v_nok" in caplog.text
         assert "v_nom" in caplog.text
         caplog.clear()
 
         # Add a generator with trailing underscore in p_nom (p_nom_)
         n.add("Generator", "gen_with_underscore", bus="test_bus", p_nom_=100.0)
-        assert "is likely a typo of standard attribute" in caplog.text
+        assert "is not a standard attribute for" in caplog.text
         assert "p_nom_" in caplog.text
         assert "p_nom" in caplog.text
 
@@ -152,8 +152,8 @@ class TestGetPotentialTypos:
         )  # Typo (should be p_nom)
 
         # Check that both warnings were logged
-        assert "are default attributes for other components" in caplog.text
-        assert "is likely a typo of standard attribute" in caplog.text
+        assert "is a standard attribute for other components" in caplog.text
+        assert "is not a standard attribute for" in caplog.text
         assert "v_nom" in caplog.text
         assert "p_nok" in caplog.text
 
@@ -188,5 +188,5 @@ class TestGetPotentialTypos:
 
             # Add a generator with unintended attribute - this should still trigger warning
             n.add("Generator", "gen_with_unintended", bus="test_bus", v_nom=380.0)
-            assert "are default attributes for other components" in caplog.text
+            assert "is a standard attribute for other components" in caplog.text
             assert "v_nom" in caplog.text
