3a1d3262efd7d12b818c204869004783c7460f36
lkstrp <lkstrp@pm.me>
2025-07-24 09:31:49 +0200

    fix: removed doctests

--- test/test_docs.py
+++ test/test_docs.py
@@ -1,8 +1,11 @@
+import doctest
 import importlib
 import pkgutil
+import sys
 
 import numpy as np
 import pandas as pd
+import pytest
 
 import pypsa
 
@@ -33,3 +36,32 @@ modules = [
     for _, name, _ in pkgutil.walk_packages(pypsa.__path__, pypsa.__name__ + ".")
     if name not in ["pypsa.utils", "pypsa.components.utils", "pypsa.typing"]
 ]
+
+
+@pytest.mark.skipif(
+    sys.version_info[:2] == (3, 10),
+    reason="Doctest fail until linopy supports numpy 2 on all python versions",
+)
+@pytest.mark.skipif(not cartopy_available, reason="Cartopy not available")
+@pytest.mark.parametrize("module", modules)
+def test_doctest(module):
+    finder = doctest.DocTestFinder()
+
+    runner = doctest.DocTestRunner(optionflags=doctest.NORMALIZE_WHITESPACE)
+
+    tests = finder.find(module)
+
+    failures = 0
+
+    for test in tests:
+        # Create a fresh copy of the globals for each test
+
+        test_globals = dict(doctest_globals)
+
+        test.globs.update(test_globals)
+
+        # Run the test
+
+        failures += runner.run(test).failed
+
+    assert failures == 0, f"{failures} doctest(s) failed in module {module.__name__}"
