f65e861d284ce18ad1c10f071d65cddc05b6e176
lkstrp <lkstrp@pm.me>
2025-07-24 12:30:43 +0200

    add better namespace for plotting helpers

--- pypsa/plot/__init__.py
+++ pypsa/plot/__init__.py
@@ -1,1 +1,17 @@
 """Plotting package for PyPSA networks."""
+
+from pypsa.plot.maps.static import (
+    add_legend_arrows,
+    add_legend_circles,
+    add_legend_lines,
+    add_legend_patches,
+    add_legend_semicircles,
+)
+
+__all__ = [
+    "add_legend_arrows",
+    "add_legend_circles",
+    "add_legend_lines",
+    "add_legend_patches",
+    "add_legend_semicircles",
+]
