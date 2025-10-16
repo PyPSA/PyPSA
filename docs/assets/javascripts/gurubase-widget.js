6772f4bc47c5860183ffc394747e7e379314cec1
Lukas Trippe <lkstrp@pm.me>
2025-10-16 09:01:07 +0200

    docs: add guru tooltip (#1403)

--- docs/assets/javascripts/gurubase-widget.js
+++ docs/assets/javascripts/gurubase-widget.js
@@ -13,12 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const widgetSettings = {
     "data-widget-id": "3a8zez8jVEVRucKLlyrKs0hKb2-xxpTg4sZkfwY17JM",
     "data-text": "Ask AI",
-    "data-margins": JSON.stringify({ bottom: "20px", right: "20px" }),
+    "data-margins": JSON.stringify({ bottom: "30px", right: "30px" }),
     "data-light-mode": "auto",
     "data-name": "PyPSA",
-    "data-icon-url": "https://raw.githubusercontent.com/PyPSA/PyPSA/f4d8576a9261becfffbf296932fcb0c3c9361b63/docs/assets/logo/logo.svg",
+    "data-icon-url": "https://raw.githubusercontent.com/PyPSA/PyPSA/master/docs/assets/logo/logo.svg",
     "data-bg-color": "#D10A49", 
-    "data-overlap-content": "true"
+    "data-overlap-content": "true",
+    "data-tooltip":
+      "Ask questions about PyPSA. Please note that questions and answers are visible anonymously to the PyPSA team and GuruBase.",
+    "data-tooltip-side": "left",
+ 
   };   
 
   // Add widget settings as data attributes
