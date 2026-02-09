04c02363d7373c21d11d3b64774633de261255b6
lkstrp <lkstrp@pm.me>
2026-02-09 17:16:38 +0100

    fix tests

--- test/test_transmission_losses.py
+++ test/test_transmission_losses.py
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: MIT
 
 
+import numpy as np
+
 import pypsa
 
 
@@ -20,17 +22,21 @@ def test_transmission_losses_independent_of_s_nom_max():
 
 def test_secant_losses_larger():
     n = pypsa.examples.ac_dc_meshed()
+    n.lines["s_nom_max"] = 2000
     n.optimize(transmission_losses={"mode": "secants"})
-    assert (
-        (n.lines_t.p0**2 * n.lines.r_pu_eff).values
-        <= n.model.solution["Line-loss"].sel(name=n.lines.index)
-    ).all()
+    true_losses = (n.lines_t.p0**2 * n.lines.r_pu_eff).values
+    model_losses = n.model.solution["Line-loss"].sel(name=n.lines.index).values
+
+    # Assert with tolerance
+    np.testing.assert_array_less(true_losses - 1e-2, model_losses)
 
 
 def test_tangent_losses_smaller():
     n = pypsa.examples.ac_dc_meshed()
+    n.lines["s_nom_max"] = 2000
     n.optimize(transmission_losses={"mode": "tangents", "segments": 3})
-    assert (
-        (n.lines_t.p0**2 * n.lines.r_pu_eff).values
-        >= n.model.solution["Line-loss"].sel(name=n.lines.index)
-    ).all()
+    true_losses = (n.lines_t.p0**2 * n.lines.r_pu_eff).values
+    model_losses = n.model.solution["Line-loss"].sel(name=n.lines.index).values
+
+    # Assert with tolerance
+    np.testing.assert_array_less(model_losses - 1e-2, true_losses)
