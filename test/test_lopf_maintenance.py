542f274b39177f53fdfefa850c9e97097dc5452f
lkstrp <lkstrp@pm.me>
2026-07-31 13:25:44 +0200

    fix: stabilize test suite

--- test/test_lopf_maintenance.py
+++ test/test_lopf_maintenance.py
@@ -553,7 +553,7 @@ def test_extendable_committable_maintainable(basic_network):
 
     maint = n.c.generators.dynamic.maintenance["gen"].values
     p = n.c.generators.dynamic.p["gen"].values
-    assert maint.sum() == 2
+    assert maint.sum() == pytest.approx(2)
     for t in range(len(maint)):
         if maint[t] > 0.5:
             assert p[t] < 1e-5
